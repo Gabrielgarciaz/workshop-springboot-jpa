cf3643fbdcc0ce7d9bb20c02c14b92370777b271
Gabriel <gabrielgarciaofc@gmail.com>
2023-01-06 17:15:20 -0300

    banco de dados h2, perfil teste, JPA

--- src/main/java/com/gabrielgarcia/curso/entities/Usuario.java
+++ src/main/java/com/gabrielgarcia/curso/entities/Usuario.java
@@ -3,9 +3,22 @@ package com.gabrielgarcia.curso.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+
+@Entity
+@Table(name = "tb_usuario")
 public class Usuario implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
@@ -16,7 +29,6 @@ public class Usuario implements Serializable{
 	
 	
 	public Usuario(Integer id, String nome, String email, String telefone, String senha) {
-		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
