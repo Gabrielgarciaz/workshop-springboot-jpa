72d2aa22bfa11177241531681813868168954e24
Gabriel <gabrielgarciaofc@gmail.com>
2023-01-11 21:24:09 -0300

    Implementando PedidoItem e PedidoItemPK. Assosiação muitos-para-muitos com dados extras Parte 1

---
+++ src/main/java/com/gabrielgarcia/curso/entities/ItemDePedido.java
@@ -0,0 +1,85 @@
+package com.gabrielgarcia.curso.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.gabrielgarcia.curso.entities.pk.ItemDePedidoPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_pedido_item")
+public class ItemDePedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private ItemDePedidoPK id;
+
+	private Integer quantidade;
+	private Double preco;
+
+	public ItemDePedido() {
+	}
+
+	public ItemDePedido(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemDePedido other = (ItemDePedido) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
